664e4180e2c2ee8661e6b94146efb5ef3e934473
tiefengeist <sahirnambiar@gmail.com>
2019-07-29 14:45:46 -0700

    Added test cases

--- wayfair.rb
+++ wayfair.rb
@@ -9,3 +9,29 @@ class LeaderBoard
     @score = score
   end
 end
+
+#TEST CASES
+
+leader_board = LeaderBoard.new
+
+leader_board.add_score(1, 50)
+
+puts(leader_board.add_score(2, 80) == 80)
+
+puts(leader_board.add_score(2, 70) == 75)
+
+puts(leader_board.add_score(2, 60) == 70)
+
+puts(leader_board.add_score(3, 90) == 90)
+
+puts(leader_board.add_score(3, 85) == 87.5)
+
+
+
+puts(leader_board.top(3) == [3, 2, 1])
+
+puts(leader_board.top(2) == [3, 2])
+
+leader_board.reset(3)
+
+puts(leader_board.top(3) == [2, 1, 3])
