db0e4c61db29c0883a7764fa505904d5a4e2e5b3
tiefengeist <sahirnambiar@gmail.com>
2019-07-31 10:59:00 -0700

    pre

--- wp.rb
+++ wp.rb
@@ -1,10 +1,6 @@
-#Annotate and then consider adding bottom functionality and timestamps for score
-#and then sort functionality for a certain time period
-
 class Player
   @@all = []
   @@ids = []
-
   attr_accessor :player_id, :scores, :total
 
   def initialize(player_id, score)
@@ -15,11 +11,15 @@ class Player
     @@all << self
   end
 
+  #didn't make getter and setter methods, since average depends
+  #on known quantities, so can use instance method
   def average
+    #this conditional is so sorting works when a player has no scores
+    #to keep types being compared the same
     if @scores.length != 0
       @average = @total.to_f / @scores.length
     else
-      @average = @total.to_f
+      @average = 0
     end
   end
 
@@ -32,6 +32,8 @@ class Player
   end
 end
 
+#score is its own class instead of part of player so we can add
+#features later if we want (lb has many p thru s)
 class Score
   @@all = []
   attr_accessor :value
@@ -39,6 +41,7 @@ class Score
     @value = value
   end
 end
+
 
 class LeaderBoard
   @@all = []
@@ -57,24 +60,32 @@ class LeaderBoard
 
   def add_score(player_id, score_value)
 
+    #check if player has scores using input id
+    #if not, create and add new player, and id
     if !@ids.include?(player_id)
       new_player = Player.new(player_id, Score.new(score_value))
       @players << new_player
       @ids << player_id
+      #if player already has scores, find player, add score, add to total
     else
       this_player = @players.find{|player| player.player_id == player_id}
       this_player.scores << Score.new(score_value)
       this_player.total += score_value
     end
+    #return the player's average
     @players.find{|player| player.player_id == player_id}.average
   end
 
-  def top(max)
+  def top(num_players)
+    #sort players by their average, since iterating over players
+    #can use instance method 'average'
     sortedPlayers = Player.all.sort_by(&:average).reverse
-    sortedPlayers.map {|player| player.player_id}.slice(0..max-1)
+    #take sorted player objects, extract ids, return "num_players" entries
+    sortedPlayers.map {|player| player.player_id}.slice(0..num_players-1)
   end
 
   def reset(player_id)
+    #find player, empty scores array, reset total
     this_player = @players.find{|player| player.player_id == player_id}
     this_player.scores = []
     this_player.total = 0
@@ -82,18 +93,23 @@ class LeaderBoard
 
 end
 
+#new leaderboard instance
 leader_board = LeaderBoard.new
-leader_board.add_score(1, 50)
+#99-104 test if add_score returns proper averages
+puts leader_board.add_score(1, 50) == 50
 puts leader_board.add_score(2, 80) == 80
 puts leader_board.add_score(2, 70) == 75
 puts leader_board.add_score(2, 60) == 70
 puts leader_board.add_score(3, 90) == 90
 puts leader_board.add_score(3, 85) == 87.5
+
+#107-109 check if top works
 puts leader_board.top(3) === [3, 2, 1]
 puts leader_board.top(2) === [3, 2]
+puts leader_board.top(1) === [3]
+
+#112-115 check if we can reset, and have add_score/average still work
 leader_board.reset(3)
 puts leader_board.top(3) == [2,1,3]
 puts leader_board.add_score(3, 600)
-puts leader_board.top(3) == [3,2,1]
-# Player.all
-# leader_board.top
+puts leader_board.top(3)
